f6012ec75cded93c283e94fd23846da471e6eb3b
Lisa <annalisa.quiamco@gmail.com>
2019-11-01 23:44:19 -0600

    Styling typical page

--- page.php
+++ page.php
@@ -14,19 +14,68 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area cf default">
-		<main id="main" class="site-main wrapper cf" role="main">
+<div id="primary" class="content-area cf">
+	<main id="main" class="site-main cf" role="main">
 
-			<?php
-			while ( have_posts() ) : the_post();
+		<?php while ( have_posts() ) : the_post(); ?>
+			<h1 style="display:none"><?php the_title(); ?></h1>
 
-				get_template_part( 'template-parts/content', 'page' );
-
-			endwhile; // End of the loop.
+			<?php if ( get_the_content() ) { ?>
+			<div class="main-text-content">
+				<div class="wrapper"><?php the_content(); ?></div>
+			</div>	
+			<?php } ?>
+			
+			<?php  
+				$sections = get_field('sections');
 			?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			<?php if ($sections) { ?>
+				
+				<?php $i=1; foreach ($sections as $s) { 
+					$title = $s['title'];
+					$subtitle = $s['subtitle'];
+					$text = $s['text'];
+					$photos = $s['photo'];
+					$countPhotos = ($photos) ? count($photos) : 0;
+					$rowClass = ($i % 2 == 0) ? 'even':'odd';
+				?>
+				<section class="section-text-image <?php echo $rowClass ?> row<?php echo $i?> <?php echo ($text || $photos) ? 'twocol':'full'; ?> cf">
+					<div class="wrapper">
+						<?php if ($text) { ?>
+						<div class="textcol">
+							<?php if ($title) { ?>
+							<h2 class="hd"><?php echo $title ?></h2>	
+							<?php } ?>
+							<?php if ($subtitle) { ?>
+							<div class="subhd"><?php echo $subtitle ?></div>	
+							<?php } ?>
+
+							<?php if ($text) { ?>
+							<div class="text"><?php echo $text ?></div>	
+							<?php } ?>
+						</div>	
+						<?php } ?>
+
+						<?php if ($photos) { ?>
+						<div class="imagecol <?php echo ($countPhotos>1) ? 'more':'one'?>">
+							<?php foreach ($photos as $pic) { ?>
+								<div class="pic"><img src="<?php echo $pic['url'] ?>" alt="<?php echo $pic['title'] ?>" /></div>
+							<?php } ?>
+						</div>	
+						<?php } ?>
+					</div>
+				</section>	
+				<?php $i++; } ?>
+
+			<?php  } ?>
+
+
+
+		<?php endwhile; ?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
 get_footer();
